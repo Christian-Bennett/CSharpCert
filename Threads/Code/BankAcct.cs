e8940db777c36e0d27553ae7f7b640417e62f017
Christian-Bennett <61510916+Christian-Bennett@users.noreply.github.com>
2020-05-08 17:48:33 -0500

    Thread Testing

---
+++ Threads/Code/BankAcct.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Code
+{
+    class BankAcct
+    {
+        private Object acctLock = new object();
+        double Balance { get; set; }
+
+        public BankAcct(double bal)
+        {
+            Balance = bal;
+
+        }
+
+        public double Withdraw(double amt)
+        {
+            if ((Balance - amt) < 0)
+            {
+                Console.WriteLine("Not enough funds");
+                return Balance;
+            }
+
+            lock (acctLock)
+            {
+                if (Balance >= amt)
+                {
+                    Console.WriteLine("Removed {0}. {1} Remaining", amt, (Balance - amt));
+                    Balance -= amt;
+                }
+                return Balance;
+            }
+
+        }
+        public void RemoveFunds()
+        {
+            Withdraw(1);
+        }
+    }
+}
--- Threads/Code/Program.cs
+++ Threads/Code/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Code
 {
@@ -6,7 +8,36 @@ namespace Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAcct acct = new BankAcct(30);
+
+            Thread[] threads = new Thread[40];
+
+            Thread.CurrentThread.Name = "main";
+
+            for(int i = 0; i < 40; i++)
+            {
+                Thread t = new Thread(new ThreadStart(acct.RemoveFunds));
+                t.Name = i.ToString();
+                threads[i] = t;
+            }
+
+            for (int i = 0; i < 40; i++)
+            {
+
+                Console.WriteLine("Thread {0} Alive : {1}", threads[i].Name, threads[i].IsAlive);
+                threads[i].Start();
+                Console.WriteLine("Thread {0} Alive : {1}", threads[i].Name, threads[i].IsAlive);
+
+
+            }
+
+            Console.WriteLine("Current Priority : {0}",
+                Thread.CurrentThread.Priority);
+
+            Console.WriteLine("Thread {0} Ending", Thread.CurrentThread.Name);
+
+                Console.ReadLine();
+
         }
     }
 }
